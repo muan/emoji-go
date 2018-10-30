a66efc9c40d9c5b25ea2f0ebd86bc33b556b4aab
Mu-An ✌️ Chiou <me@muanchiou.com>
2018-10-30 11:15:16 -0400

    format things and fetch the JSON file from package
    Co-authored-by: Kristján Oddsson <koddsson@github.com>

--- emoji.go
+++ emoji.go
@@ -1,54 +1,79 @@
 package main
 
-import "os"
-import "fmt"
-import "encoding/json"
-import "io/ioutil"
-import s "strings"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	s "strings"
+)
 
 type emoji struct {
-  Keywords []string
-  Char string
-  Fitzpatrick_scale bool
-  Category string
+	Keywords         []string
+	Char             string
+	FitzpatrickScale bool
+	Category         string
 }
 
-func main()  {
-  if len(os.Args) != 2 {
-    fmt.Println("Please provide one keyword.")
-  } else {
-    query := os.Args[1]
-    data := getLib()
-    fmt.Println("Keyword:", query)
-    var results []string
-    for key, item := range data {
-      if s.Index(key, query) >= 0 {
-        if key == query {
-          results = append([]string{item.Char}, results...)
-        } else {
-          results = append(results, item.Char)
-        }
-      }
-    }
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Please provide one keyword.")
+	} else {
+		query := os.Args[1]
+		data := getLib()
+		fmt.Println("Keyword:", query)
+		var results []string
+		for key, item := range data {
+			if s.Index(key, query) >= 0 {
+				if key == query {
+					results = append([]string{item.Char}, results...)
+				} else {
+					results = append(results, item.Char)
+				}
+			}
+		}
 
-    if len(results) > 0 {
-      fmt.Println("Found:")
-      for i, emoji := range results {
-        if i < 5 {
-          fmt.Println(i + 1, emoji)
-        } else {
-          break
-        }
-      }
-    } else {
-      fmt.Println("No emoji found 😭.")
-    }
-  }
+		if len(results) > 0 {
+			fmt.Println("Found:")
+			for i, emoji := range results {
+				if i < 5 {
+					fmt.Println(i+1, emoji)
+				} else {
+					break
+				}
+			}
+		} else {
+			fmt.Println("No emoji found 😭.")
+		}
+	}
 }
 
 func getLib() map[string]emoji {
-  lib, _ := ioutil.ReadFile("./emoji.json")
-  var data map[string]emoji
-  json.Unmarshal([]byte(string(lib)), &data)
-  return data
+	lib, err := ioutil.ReadFile("./emoji.json")
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Println("No emoji.json found. Fetching..")
+			lib = fetchLib()
+			ioutil.WriteFile("emoji.json", lib, 0777)
+		} else {
+			fmt.Println(err)
+		}
+	}
+
+	var data map[string]emoji
+	json.Unmarshal([]byte(string(lib)), &data)
+	return data
+}
+
+func fetchLib() []byte {
+	resp, err := http.Get("https://raw.githubusercontent.com/muan/emojilib/master/emojis.json")
+	if err != nil {
+		fmt.Println("ERRRRR")
+		fmt.Println(err)
+	}
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	return body
 }
